837f00e2c1337ebf323117686e345081c22a031e
Vinicius-Tavares-Silva <vini_tavares10@hotmail.com>
2024-05-11 11:50:41 -0300

    add main

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -28,3 +29,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
